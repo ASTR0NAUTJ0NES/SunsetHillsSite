78aa5507fb313e7ed1612540405e046c337e1944
Andrew-P-Albanese <astr0nautj0nesapa@gmail.com>
2020-10-08 20:32:57 -0400

    updated sunset hills logic

--- SunsetHillsSite/Scripts/custom.js
+++ SunsetHillsSite/Scripts/custom.js
@@ -1,1 +1,24 @@
 // Custom JS
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+    let inputArr = [parseInt(document.getElementById("input1").value),
+        parseInt(document.getElementById("input2").value),
+        parseInt(document.getElementById("input3").value),
+        parseInt(document.getElementById("input4").value),
+        parseInt(document.getElementById("input5").value)];
+
+    let outputArr = [];
+    let sunnyBuildings = 1;
+    let currentTallest = inputArr[0];
+    outputArr.push(currentTallest);
+
+    for (let loop = 0; loop < inputArr.length; loop++) {
+        if (inputArr[loop] > currentTallest) {
+            sunnyBuildings++;
+            outputArr += inputArr[loop];
+            currentTallest = inputArr[loop];
+            document.getElementById("building" + outputArr[loop] + "Img").style.visibility = "visible";
+        }
+        document.getElementById("resultSpan").innerHTML = `${sunnyBuildings} buildings have sun cover. <br /> The buildings that have sun cover are: ${outputArr.join("")}.`;
+    }
+});
